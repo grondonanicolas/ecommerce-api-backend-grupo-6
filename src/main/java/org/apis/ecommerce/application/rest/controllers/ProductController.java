93f057d8af58bd03bd88ab1c63f22ace97f7dd20
Alan Tapia <alan.tapia@rcpo.com.ar>
2024-08-26 22:00:50 -0300

    ft: agrego dependencia spring-boot-starter-web creo controller productos

---
+++ src/main/java/org/apis/ecommerce/application/rest/controllers/ProductController.java
@@ -0,0 +1,26 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public String listProducts(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) Boolean recentlyViewed) {
+
+        return "Listando productos - Categoría: " + category + ", Vistos recientemente: " + recentlyViewed;
+    }
+
+
+    @GetMapping("/{productId}")
+    public String getProductDetail(@PathVariable Long id) {
+
+        return "Detalles del producto con ID: " + id;
+    }
+    
+}
