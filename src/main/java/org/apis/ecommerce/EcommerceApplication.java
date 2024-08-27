b6438282fbc34050c8502e884e80482265090827
Alan Tapia <alan.tapia@rcpo.com.ar>
2024-08-26 22:01:13 -0300

    fix: corrijo path

--- src/main/java/org/apis/ecommerce/EcommerceApplication.java
+++ src/main/java/org/apis/ecommerce/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package org.apis.ecommerce.ecommerce;
+package org.apis.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
