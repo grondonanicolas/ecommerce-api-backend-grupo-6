a5d1ed08fe1385c84e086c986784dd8b590466bb
Alan Tapia <alan.tapia@rcpo.com.ar>
2024-09-21 17:18:41 -0300

    quito relacion

--- src/main/java/org/apis/ecommerce/domain/models/Product.java
+++ src/main/java/org/apis/ecommerce/domain/models/Product.java
@@ -34,9 +34,6 @@ public class Product {
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
-    @OneToOne
-    private Outstanding outstanding;
-
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
