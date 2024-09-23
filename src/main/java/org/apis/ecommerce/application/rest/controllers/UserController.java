85ab0846e443b56b91290ad07caa4b47c844f149
Alan Tapia <alan.tapia@rcpo.com.ar>
2024-09-22 23:27:26 -0300

    controller for Favourite & Historic

--- src/main/java/org/apis/ecommerce/application/rest/controllers/UserController.java
+++ src/main/java/org/apis/ecommerce/application/rest/controllers/UserController.java
@@ -3,12 +3,23 @@ package org.apis.ecommerce.application.rest.controllers;
 
 import lombok.AllArgsConstructor;
 import org.apis.ecommerce.application.rest.dtos.UserDTO;
+import org.apis.ecommerce.application.rest.dtos.FavouriteDTO;
+import org.apis.ecommerce.application.rest.dtos.HistoricDTO;
+import org.apis.ecommerce.application.rest.dtos.OutstandingDTO;
+import org.apis.ecommerce.application.rest.dtos.ProductDTO;
+import org.apis.ecommerce.domain.models.Product;
+import org.apis.ecommerce.domain.models.User;
 import org.apis.ecommerce.domain.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -21,4 +32,42 @@ public class UserController {
         UserDTO user = userService.getUserById(id);
         return ResponseEntity.ok(user);
     }
+
+    @PostMapping("/historic")
+    public void addProductHistoric(@RequestBody HistoricDTO historic, @AuthenticationPrincipal User user) throws Exception {
+        userService.addProductHistoric(historic.getProductId(), user);
+    }
+
+    @GetMapping("/historic")
+    public List<ProductDTO> getHistoricByUser(@AuthenticationPrincipal User user) throws Exception{
+        List<Product> products = userService.getProductHistoric(user);
+        return products.stream()
+                .map(product -> 
+                        new ProductDTO(
+                            product.getId(), 
+                            product.getDescription(), 
+                            product.getPricePerUnit(), 
+                            product.getCurrentStock()))
+                .toList();
+    }
+
+    @GetMapping("/favourite")
+    public List<ProductDTO> getFavouriteByUser(@AuthenticationPrincipal User user) throws Exception{
+        List<Product> products = userService.getProductHistoric(user);
+        return products.stream()
+                .map(product -> 
+                        new ProductDTO(
+                            product.getId(), 
+                            product.getDescription(), 
+                            product.getPricePerUnit(), 
+                            product.getCurrentStock()))
+                .toList();
+    }
+
+
+    @PostMapping("/historic")
+    public void addProductFavourite(@RequestBody FavouriteDTO favourite, @AuthenticationPrincipal User user) throws Exception {
+        userService.addProductHistoric(favourite.getProductId(), user);
+    }
+
 }
--- src/main/java/org/apis/ecommerce/domain/services/UserService.java
+++ src/main/java/org/apis/ecommerce/domain/services/UserService.java
@@ -2,20 +2,68 @@ package org.apis.ecommerce.domain.services;
 
 
 import org.apis.ecommerce.application.rest.dtos.UserDTO;
+import org.apis.ecommerce.domain.models.Favourite;
+import org.apis.ecommerce.domain.models.Historic;
+import org.apis.ecommerce.domain.models.Product;
 import org.apis.ecommerce.domain.models.User;
+import org.apis.ecommerce.domain.repositories.IProductRepository;
 import org.apis.ecommerce.infrastructure.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private IProductRepository productRepository;
+
     public UserDTO getUserById(Long id) throws Exception {
         var userName = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userRepository.findById(id).orElseThrow(() -> new Exception("An error has ocurred"));
         return new UserDTO(user.getId(), user.getUsername(), user.getEmail());
     }
+
+    public void addProductHistoric(Integer productId, @AuthenticationPrincipal User user) throws Exception {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new Exception("Producto no encontrado"));
+        Historic historic = user.getHistoric();
+
+        if(!historic.getProducts().contains(product))
+        historic.getProducts().add(product);
+        userRepository.save(user);
+    }
+
+    public void addProductFavourite(Integer productId, User user) throws Exception {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new Exception("Producto no encontrado"));
+        Favourite favourite = user.getFavourite();
+
+        if(!favourite.getProducts().contains(product))
+        favourite.getProducts().add(product);
+        userRepository.save(user);
+    }
+
+    public void removeProductFavourite(Integer productId, User user) throws Exception {
+        Product product = productRepository.findById(productId)
+            .orElseThrow(() -> new Exception("Producto no encontrado"));
+    
+        Favourite favourite = user.getFavourite();
+    
+        if (favourite.getProducts().contains(product)) {
+            favourite.getProducts().remove(product);
+        }
+        userRepository.save(user);
+    }
+
+    public List<Product> getProductHistoric(User user){
+        return user.getHistoric().getProducts();
+    }
+
+    public List<Product> getProductFavourites(User user){
+        return user.getFavourite().getProducts();
+    }
+
 }
