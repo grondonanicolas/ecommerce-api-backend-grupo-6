748027b7d9bf29fc14366b53356f1363775d66ef
Alan Tapia <alan.tapia@rcpo.com.ar>
2024-09-22 22:56:04 -0300

    generate favourites products & historic products models

--- src/main/java/org/apis/ecommerce/domain/models/Historic.java
+++ src/main/java/org/apis/ecommerce/domain/models/Historic.java
@@ -3,9 +3,13 @@ package org.apis.ecommerce.domain.models;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.Data;
+import lombok.Setter;
+import lombok.Getter;
+import java.util.List;
 
-@Data
+
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -14,4 +18,14 @@ public class Historic {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @ManyToMany
+    @JoinTable(name="historic_product",
+                joinColumns = @JoinColumn(name="historic_id"),
+                inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<Product> products;
 }
